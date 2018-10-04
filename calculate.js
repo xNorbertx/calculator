ce79a8593d9fe33c9a5b4b9535d6e14f77c03a23
xNorbertx <norbert.g.e.bakker@gmail.com>
2018-10-04 22:49:09 +0200

    Updated all files

--- calculate.js
+++ calculate.js
@@ -1,64 +1,108 @@
-var MODE;
-var PREV_VAL;
-var DISPLAY;
+$(".btn").on("click", function() {
+  $(".display-screen").attr('class', tmp + ' blip');
+});
 
-function set(val) {
+var tmp = "display-screen";
+var DISPLAY;
+var CALCULATION = {
+  first: null,
+  operation: null,
+  second: null,
+  prev: null,
+  active: null
+};
+
+function setCalculation(val) {
+  if (CALCULATION.active === 'first') {
+    CALCULATION.first = parseFloat(val.replace(",", "."));
+  } else {
+    CALCULATION.second = parseFloat(val.replace(",", "."));
+  }
+}
+
+function setNumber(val) {
 	var displayField = $(".display-screen");
-  if ((displayField.val() === "0" && val !== ",") || MODE) {
+  $("#clear").val("C");
+  if (val === "," && CALCULATION.prev) {
+    DISPLAY = "0" + val;
+  } else if (displayField.val() === "0" && val !== ","
+      || CALCULATION.prev === 'arit'
+      || CALCULATION.prev === 'calc') {
     DISPLAY = val;
   } else {
     DISPLAY = displayField.val() + val;
   }
+  setCalculation(DISPLAY);
+  CALCULATION.prev = null;
 	displayField.val(DISPLAY);
 }
 
 function cleardisplay() {
   $(".display-screen").val(0);
-  MODE = null;
-  PREV_VAL = null;
+  $("#clear").val("AC");
   DISPLAY = "0";
+  CALCULATION.first = null;
+  CALCULATION.operation = null;
+  CALCULATION.second = null;
+  CALCULATION.prev = null;
+  CALCULATION.active = 'first';
 }
 
-function action(val) {
-  MODE = val;
-  DISPLAY = $(".display-screen").val();
-  if (DISPLAY) {
-    DISPLAY = DISPLAY.replace(",", ".");
-    PREV_VAL = parseFloat(DISPLAY);
-  }
+function arithmeticOperation(val) {
+  CALCULATION.operation = val;
+  CALCULATION.prev = 'arit';
+  CALCULATION.active = 'second';
 }
 
 function reverseAbsolute() {
   var res = parseFloat(DISPLAY.replace(",", "."));
   res = -res;
-  DISPLAY = res.toString();
+  if ( CALCULATION.active === 'first') {
+    CALCULATION.first = res;
+  } else {
+    CALCULATION.second = res;
+  }
+  DISPLAY = res.toString().replace(".", ",");
   $(".display-screen").val(DISPLAY);
 }
 
 function calculate() {
-  if (PREV_VAL && MODE) {
+  CALCULATION.prev = 'calc';
+  if (CALCULATION.operation) {
     var res;
-    switch (MODE) {
+    switch (CALCULATION.operation) {
       case 'add':
-        res = PREV_VAL + parseFloat(DISPLAY);
+        res = CALCULATION.first + CALCULATION.second;
         break;
       case 'subtract':
-        res = PREV_VAL - parseFloat(DISPLAY);
+        res = CALCULATION.first - CALCULATION.second;
         break;
       case 'multiply':
-        res = PREV_VAL * parseFloat(DISPLAY);
+        res = CALCULATION.first * CALCULATION.second;
         break;
       case 'divide':
-        res = PREV_VAL / parseFloat(DISPLAY);
-        break;
-      case 'percentage':
-        res = parseFloat(DISPLAY) / 100;
+        res = CALCULATION.first / CALCULATION.second;
         break;
       default:
         res = 0;
         break;
     }
+    CALCULATION.first = res;
+    CALCULATION.active = 'first';
     DISPLAY = res.toString().replace(".", ",");
     $(".display-screen").val(DISPLAY);
   }
+}
+
+function calculatePercentage() {
+  var res;
+  if ( CALCULATION.active === 'first') {
+    res = CALCULATION.first / 100
+    CALCULATION.first = res;
+  } else if ( CALCULATION.active === 'second' ){
+    res = CALCULATION.second / 100;
+    CALCULATION.second = res;
+  }
+  DISPLAY = res.toString().replace(".", ",");
+  $(".display-screen").val(DISPLAY);
 }
